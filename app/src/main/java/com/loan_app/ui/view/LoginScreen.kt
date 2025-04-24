bc492a6feaa397808ca70445a38366aa00980308
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-24 17:00:06 +0000

    Update

--- app/src/main/java/com/loan_app/ui/view/LoginScreen.kt
+++ app/src/main/java/com/loan_app/ui/view/LoginScreen.kt
@@ -4,13 +4,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.loan_app.ui.viewmodel.LoginViewModel
 
@@ -24,11 +29,24 @@ fun LoginScreen(viewModel: LoginViewModel = viewModel()){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        TextField(
+        OutlinedTextField(
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFFEB5757),     // background when focused
+                unfocusedContainerColor = Color(0XFFE5E5F0),   // background when not focused
+                focusedTextColor = Color(0XFFFFFFFF),
+//                unfocusedTextColor = Color.Green
+            ),
             value = viewModel.msisdn.value,
 //            onValueChange = viewModel::onMsisdnChanged, //input -> viewModel.onMsisdnChanged(input
             onValueChange = { input -> viewModel.onMsisdnChanged(input) },
-            label = { Text("Phone Number") }
+            label = { Text("Phone Number") },
+            leadingIcon = {
+                Text(text = "+233 |")
+            },
+            textStyle = TextStyle(
+                color = Color.Green,
+                fontSize = 20.sp
+            )
         )
     }
 }
