7179fe376a88786b46f161d565b66f5abbb51b4f
Foster Amponsah <foster.asante@gwosevo.com>
2025-04-25 00:04:50 +0000

    Update

--- app/src/main/java/com/loan_app/ui/view/LoginScreen.kt
+++ app/src/main/java/com/loan_app/ui/view/LoginScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,6 +23,20 @@ import com.loan_app.ui.viewmodel.LoginViewModel
 @Preview(showBackground = true)
 @Composable
 fun LoginScreen(viewModel: LoginViewModel = viewModel()){
+//fun LoginScreen(viewModel: LoginViewModel = viewModel()){
+    /*
+    This line below observes the msisdn state variable defined inside the LoginViewModel. It will cause
+    the ui/view to be re-build any time the state value changes inside the LoginViewModel
+    */
+viewModel.msisdn.observeAsState().value;
+
+    /*
+    The line below retrieves the state msisdn value from the LoginViewModel in form that cannot be
+    changed in the view.
+
+    NOTE: Changes to state variable must occur only in the view model
+    */
+val msisdnData = viewModel.getMsisdn();
 
     Column(modifier = Modifier
         .fillMaxSize()
@@ -31,15 +46,16 @@ fun LoginScreen(viewModel: LoginViewModel = viewModel()){
     ) {
         OutlinedTextField(
             colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = Color(0xFFEB5757),     // background when focused
+                focusedContainerColor = Color(0XFFFFFFFF),     // background when focused
                 unfocusedContainerColor = Color(0XFFE5E5F0),   // background when not focused
-                focusedTextColor = Color(0XFFFFFFFF),
+//                focusedTextColor = Color(0xFFEB5757),
+                focusedPlaceholderColor = Color(0xFFEB5757)
 //                unfocusedTextColor = Color.Green
             ),
-            value = viewModel.msisdn.value,
+            value = "${msisdnData.value}",
 //            onValueChange = viewModel::onMsisdnChanged, //input -> viewModel.onMsisdnChanged(input
             onValueChange = { input -> viewModel.onMsisdnChanged(input) },
-            label = { Text("Phone Number") },
+//            label = { Text("Phone Number") },
             leadingIcon = {
                 Text(text = "+233 |")
             },
--- app/src/main/java/com/loan_app/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/loan_app/ui/viewmodel/LoginViewModel.kt
@@ -1,23 +1,24 @@
 package com.loan_app.ui.viewmodel
 
 import android.util.Log
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.loan_app.data.model.Post
-import com.loan_app.data.model.PostPostData
-import com.loan_app.data.repository.PostRepository
-import kotlinx.coroutines.launch
 
 class LoginViewModel () : ViewModel() {
-    val msisdn = mutableStateOf("");
+//    val msisdn = mutableStateOf("");
+    val msisdn = MutableLiveData("");
 
     fun onMsisdnChanged(newMsisdn: String) {
         msisdn.value = newMsisdn
         Log.i("LoginScreen", "CURRENT TEXT: $newMsisdn")
+    }
+
+    /*
+    This function returns the msisdn state variable in a form that cannot be changed inside the view
+    NOTE: MutableLiveData allows state variables to be change whereas LiveData does not
+    */
+    fun  getMsisdn(): LiveData<String>{
+        return msisdn;
     }
 }
