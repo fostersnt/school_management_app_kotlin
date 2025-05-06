a4865005ca5bc54ac8b2fce23043dbc257391f1b
Foster Amponsah <foster.asante@gwosevo.com>
2025-05-06 13:08:20 +0000

    Update

--- app/src/main/java/com/loan_app/ui/view/user/ProfileScreen.kt
+++ app/src/main/java/com/loan_app/ui/view/user/ProfileScreen.kt
@@ -17,30 +17,26 @@ import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.OutlinedButton
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.loan_app.ui.viewmodel.LoginViewModel
 
 //@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 //fun ProfileScreen(navController: NavController){
 //
